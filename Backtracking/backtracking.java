93e9080d8ffa34b4ad66fba7af8da42f0a5de749
Aryan Raj <62373648+aryan-139@users.noreply.github.com>
2023-05-28 21:17:04 +0530

    Create backtracking.java

---
+++ Backtracking/backtracking.java
@@ -0,0 +1,12 @@
+/*Backtracking 
+ 
+So basically what is happening in the Backtracking is that we are using brute force approach and trying to tackle all the cases that we may encounter and only then do we get the solution.
+
+When we draw a tree of the results that we may encounter while trying to approach the problem, we call this tree as the state-space tree;
+
+Now, when we have a limiting condition on as to how we have to get the results then this is called as the Bounding Function.
+
+Example in Copy;
+ */
+
+ 
